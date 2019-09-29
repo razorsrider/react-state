320deddbb9d2ce625a3c59c63e617ccafc658118
razorsrider <razorsrider@gmail.com>
2019-09-30 00:28:43 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import {Router, Route, Switch} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import main from "./contents/main/main";
+import content from "./contents/content/content";
+import about from "./contents/about/about";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xl-12 col-lg-12 col-sm-12 col-md-12 col-xs-12 col-12'>
+                        <Header/>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <Router history={createBrowserHistory()}>
+                        <div className='col-xl-2 col-lg-2 col-sm-12 col-md-12 col-xs-12 col-12'
+                             style={{marginTop: '10px'}}>
+                            <Navigation/>
+                        </div>
+                        <div className='col-xl-10 col-lg-10 col-sm-12 col-md-12 col-xs-12 col-12'>
+
+                            <Switch>
+                                <Route path='/' exact component={main}/>
+                                <Route path='/data' exact component={content}/>
+                                <Route path='/about' exact component={about}/>
+                            </Switch>
+
+                        </div>
+                    </Router>
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
